106241a13325fc68cd97e21c8aa96c056512e2d5
Arteks718 <artexbro.official@gmail.com>
2024-12-18 22:21:08 +0200

    Add time formatter function

--- client/src/components/Events/EventsList/EventItem/EventItem.jsx
+++ client/src/components/Events/EventsList/EventItem/EventItem.jsx
@@ -1,27 +1,49 @@
-import React from 'react'
-import { intervalToDuration } from 'date-fns'
-import styles from './EventItem.module.sass'
+import React, { useState, useEffect } from 'react';
+import {
+  differenceInBusinessDays,
+  differenceInHours,
+  differenceInMinutes,
+  differenceInMonths,
+  differenceInSeconds,
+  differenceInYears,
+  intervalToDuration,
+  roundToNearestMinutes,
+  formatDuration,
+} from 'date-fns';
+import styles from './EventItem.module.sass';
 
 const EventItem = (props) => {
-  const formatTime = (totalSeconds) => {
-    // Перетворюємо загальну кількість секунд на тривалість
-    const duration = intervalToDuration({ start: 0, end: totalSeconds * 1000 });
-  
-    // Форматуємо тривалість у потрібний формат
-    const formattedTime = `${duration.hours}h ${duration.minutes}m ${duration.seconds}s`;
-    
-    return formattedTime;
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const test = [{ name: 'h' }];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+  }, [currentTime]);
+  const formatTime = (date) => {
+    return formatDuration({
+      years: differenceInYears(date, currentTime),
+      months: differenceInMonths(date, currentTime),
+      days: differenceInBusinessDays(date, currentTime),
+      hours: differenceInHours(date, currentTime) % 24,
+      minutes: differenceInMinutes(date, currentTime) % 60,
+      seconds: differenceInSeconds(date, currentTime) % 60,
+    })
+      .replace(
+        /\b(years?|months?|days?|hours?|minutes?|seconds?)\b/g,
+        (match) => match[0]
+      )
+      .replace(/(\d)\s+([a-zA-Z])/g, '$1$2');
   };
 
   const { name, finishDate, reminderDate, startDate } = props.event;
   return (
     <div className={styles.item}>
       <h3>{name}</h3>
-      <p className={styles.time}>
-        {formatTime(new Date(formatTime).getTime())}
-      </p>
+      <p className={styles.time}>{formatTime(finishDate)}</p>
     </div>
-  )
-}
+  );
+};
 
-export default EventItem
+export default EventItem;

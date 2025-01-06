614c228e987b05229c6718abdb6387c540f91b95
Arteks718 <artexbro.official@gmail.com>
2025-01-06 15:16:12 +0200

    Add icon to remaining block

--- client/src/components/Events/EventsList/EventsList.jsx
+++ client/src/components/Events/EventsList/EventsList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import styles from './EventsList.module.sass';
 import EventItem from './EventItem/EventItem';
 
@@ -10,14 +11,14 @@ const EventsList = (props) => {
     .partition(event => new Date(event.finishDate).getTime() >= new Date().getTime())
     .flatMap(partition => partition)
     .value();
-    
+
   return (
     <div className={styles.container}>
       <div className={styles.titleBlock}>
         <h2>Live upcoming checks</h2>
-        <div>
-          <p className={styles.remaining}>Remaining time</p>
-          <img src="" alt="" />
+        <div className={styles.remainingBlock}>
+          <p className={styles.remainingText}>Remaining time</p>
+          <AccessTimeIcon />
         </div>
       </div>
       <div className={styles.eventsList}>

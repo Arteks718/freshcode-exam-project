1150299954af28a6f79aa8c388d36caa199e7d81
Arteks718 <artexbro.official@gmail.com>
2025-01-09 00:41:18 +0200

    Create custom hook of useCurrentDate

---
+++ client/src/hooks/useCurrentDate.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+
+const useCurrentDate = (interval = 1000) => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentDate(new Date());
+    }, interval);
+    return () => clearInterval(timer);
+  }, [interval]);
+
+  return currentDate;
+}
+
+export default useCurrentDate

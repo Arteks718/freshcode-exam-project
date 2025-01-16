c50a3adbcf714bfbb03f3c023f11c114c737c599
Arteks718 <artexbro.official@gmail.com>
2025-01-16 22:48:46 +0200

    Fix redirect in PrivateHoc

--- client/src/components/PrivateHoc/PrivateHoc.jsx
+++ client/src/components/PrivateHoc/PrivateHoc.jsx
@@ -3,35 +3,33 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { getUser } from '../../store/slices/userSlice';
 import Spinner from '../Spinner/Spinner';
+import CONSTANTS from '../../constants';
 
-const PrivateHoc = Component => {
-  
-  const Hoc = props => {
-    const { data, getUser, history, match, isFetching } = props;
+const PrivateHoc = (Component) => {
+  const Hoc = (props) => {
+    const { data, getUser, history, match, isFetching, error } = props;
+    const token = localStorage.hasOwnProperty(CONSTANTS.ACCESS_TOKEN);
 
     useEffect(() => {
-      if(!data){
-        getUser();
-        // history.replace('/login');
-        return;
+      if (!token) {
+        return <Redirect to="/login" />;
       }
-    }, [])
 
+      if (!data) {
+        getUser();
+      }
+    }, [getUser, data, token]);
 
     return (
       <>
         {isFetching ? (
           <Spinner />
         ) : (
-          <Component
-            history={history}
-            match={match}
-            {...props}
-          />
+          <Component history={history} match={match} {...props} />
         )}
       </>
     );
-  }
+  };
 
   const mapStateToProps = (state) => state.userStore;
 

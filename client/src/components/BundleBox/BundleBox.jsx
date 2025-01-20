4a25eccbfc2a6ad342a81350f363775a5494e683
Arteks718 <artexbro.official@gmail.com>
2025-01-20 22:53:12 +0200

    Refactor BundleBox

--- client/src/components/BundleBox/BundleBox.jsx
+++ client/src/components/BundleBox/BundleBox.jsx
@@ -1,54 +1,50 @@
-import React from 'react';
+import React, { useMemo, useRef } from 'react';
 import styles from './BundleBox.module.sass';
 import CONSTANTS from '../../constants';
 
-const BundleBox = props => {
+const BundleBox = (props) => {
+  const { setBundle, header, path, describe } = props;
+  const bundleRef = useRef(null);
   const defaultPathToImages = `${CONSTANTS.STATIC_IMAGES_PATH}contestLabels/`;
 
-  const renderImage = () => {
-    const array = [];
-    for (let i = 0; i < props.path.length; i++) {
-      array.push(
-        <img
-          src={defaultPathToImages + props.path[i]}
-          key={i}
-          className={styles.imgContainer}
-          alt={props.path[i].replace(/.png/g, 'Contest')}
-        />
-      );
-    }
-    return array;
-  };
+  const renderImage = useMemo(() => {
+    return path.map((p, i) => (
+      <img
+        src={defaultPathToImages + p}
+        key={i}
+        className={styles.imgContainer}
+        alt={p.replace(/.png/g, 'Contest')}
+      />
+    ));
+  }, [path, defaultPathToImages]);
 
   const mouseOverHandler = () => {
-    const element = document.getElementById(props.header);
-    for (let i = 0; i < element.children[0].children.length; i++) {
-      element.children[0].children[
-        i
-      ].src = `${defaultPathToImages}blue_${props.path[i]}`;
-    }
+    const element = bundleRef.current;
+    Array.from(element.children[0].children).forEach((img, i) => {
+      img.src = `${defaultPathToImages}blue_${path[i]}`;
+    });
   };
 
   const mouseOutHandler = () => {
-    const element = document.getElementById(props.header);
-    for (let i = 0; i < element.children[0].children.length; i++) {
-      element.children[0].children[i].src = defaultPathToImages + props.path[i];
-    }
+    const element = bundleRef.current;
+    Array.from(element.children[0].children).forEach((img, i) => {
+      img.src = defaultPathToImages + path[i];
+    });
   };
 
   const getBackClass = () =>
-    props.path.length === 1 ? ' ' : ` ${styles.combinedBundle}`;
+    path.length === 1 ? ' ' : ` ${styles.combinedBundle}`;
 
-  const { setBundle, header, describe } = props;
   return (
     <div
       onMouseOver={mouseOverHandler}
       onMouseOut={mouseOutHandler}
       onClick={() => setBundle(header)}
+      ref={bundleRef}
       id={header}
       className={styles.bundleContainer + getBackClass()}
     >
-      <div>{renderImage()}</div>
+      <div>{renderImage}</div>
       <div className={styles.infoContainer}>
         <span className={styles.bundleName}>{header}</span>
         <hr />

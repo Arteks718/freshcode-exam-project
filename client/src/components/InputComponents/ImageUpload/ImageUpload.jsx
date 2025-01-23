8ed0069064dad2c31e8ecc1c53565ba67e49d944
Arteks718 <artexbro.official@gmail.com>
2025-01-23 23:50:26 +0200

    Fix upload file in logo offer

--- client/src/components/InputComponents/ImageUpload/ImageUpload.jsx
+++ client/src/components/InputComponents/ImageUpload/ImageUpload.jsx
@@ -1,41 +1,51 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import classNames from 'classnames';
 import { useField } from 'formik';
 
-const ImageUpload = props => {
-  const [field, meta, helpers] = useField(props.name);
+const ImageUpload = (props) => {
+  const [{ value, ...field }, , helpers] = useField(props.name);
   const { uploadContainer, inputContainer, imgStyle } = props.classes;
-  const onChange = e => {
-    const node = window.document.getElementById('imagePreview');
+  const imagePreviewRef = useRef(null);
+
+  const onChange = useCallback((e) => {
     const file = e.target.files[0];
     const imageType = /image.*/;
+
     if (!file.type.match(imageType)) {
       e.target.value = '';
+      return
     } else {
-      field.onChange(file);
+      helpers.setValue(file);
       const reader = new FileReader();
       reader.onload = () => {
-        node.src = reader.result;
+        if (imagePreviewRef.current) {
+          imagePreviewRef.current.src = reader.result;
+        }
+      };
+      reader.onerror = () => {
+        console.error('An error occurred while reading the file.');
       };
       reader.readAsDataURL(file);
     }
-  };
+  }, [helpers]);
+
   return (
     <div className={uploadContainer}>
       <div className={inputContainer}>
-        <span>Support only images (*.png, *.gif, *.jpeg)</span>
+        <span>Support only images (*.png, *.gif, *.jpg, *.jpeg)</span>
         <input
           {...field}
           id="fileInput"
           type="file"
           accept=".jpg, .png, .jpeg"
-          onClick={onChange}
+          onChange={onChange}
         />
-        <label htmlFor="fileInput">Chose file</label>
+        <label htmlFor="fileInput">Choose file</label>
       </div>
       <img
         id="imagePreview"
-        className={classNames({ [imgStyle]: !!field.value })}
+        ref={imagePreviewRef}
+        className={classNames({ [imgStyle]: !!value })}
         alt="user"
       />
     </div>

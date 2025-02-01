94a865c273b52ac8b783668f31b9c2e6feff1d6e
Arteks718 <artexbro.official@gmail.com>
2025-02-01 22:57:09 +0200

    Create Catalog model

---
+++ server/src/db/migrations/20250201215924-create-catalogs.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Catalogs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      catalogName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      upgradeAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      }
+    });
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Catalogs');
+  }
+};
---
+++ server/src/db/models/Catalog.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('Catalogs', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
+    catalogName: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+    upgradeAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+  })
+}

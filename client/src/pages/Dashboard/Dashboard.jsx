67d1e187617e1021ff5c5465ef3e281ce41d809f
Arteks718 <artexbro.official@gmail.com>
2025-02-12 17:40:25 +0200

    Update Dashboard

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -3,22 +3,27 @@ import { connect } from 'react-redux';
 import CONSTANTS from '../../constants';
 import CustomerDashboard from '../../components/CustomerDashboard/CustomerDashboard';
 import CreatorDashboard from '../../components/CreatorDashboard/CreatorDashboard';
+import ModeratorDashboard from '../../components/ModeratorDashboard/ModeratorDashboard';
 import Header from '../../components/Header/Header';
 
-const Dashboard = props => {
-  const { role, history } = props;
+const dashboardComponents = {
+  [CONSTANTS.CUSTOMER]: CustomerDashboard,
+  [CONSTANTS.CREATOR]: CreatorDashboard,
+  [CONSTANTS.MODERATOR]: ModeratorDashboard,
+};
+
+const Dashboard = (props) => {
+  const { role, history, match } = props;
+  const DashboardComponent = dashboardComponents[role] || (() => <div>No dashboard available</div>);
+
   return (
     <div>
       <Header />
-      {role === CONSTANTS.CUSTOMER ? (
-        <CustomerDashboard history={history} match={props.match} />
-      ) : (
-        <CreatorDashboard history={history} match={props.match} />
-      )}
+      <DashboardComponent history={history} match={match} />
     </div>
   );
 };
 
-const mapStateToProps = state => state.userStore.data;
+const mapStateToProps = (state) => state.userStore.data;
 
 export default connect(mapStateToProps)(Dashboard);

1579a50372cfd25cc8c870f4b4e03cd06bdc2b66
Arteks718 <artexbro.official@gmail.com>
2025-02-16 17:09:07 +0200

    Add 'empty list' notification

--- client/src/components/OffersContainer/OffersContainer.jsx
+++ client/src/components/OffersContainer/OffersContainer.jsx
@@ -3,11 +3,14 @@ import styles from './OffersContainer.module.sass';
 
 const OffersContainer = (props) => {
   const { children } = props;
-  console.log(children);
+
   return (
     <div className={styles.offerContainer}>
-      {/* <span className={styles.title}>Offers list</span> */}
-      <div className={styles.offersList}>{children}</div>
+      {children.length !== 0 ? (
+        <div className={styles.offersList}>{children}</div>
+      ) : (
+        <div className={styles.emptyNotification}>At the moment, list of offers is empty!</div>
+      )}
     </div>
   );
 };

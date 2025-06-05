2cf9f6840b2a42c319fb63d76519efedd7bdb3a5
Arteks718 <artexbro.official@gmail.com>
2025-06-05 16:10:53 +0300

    Refactors profile option tabs into reusable function

--- client/src/pages/UserProfile/UserProfile.jsx
+++ client/src/pages/UserProfile/UserProfile.jsx
@@ -20,6 +20,19 @@ const UserProfile = (props) => {
     cashOut,
   } = props;
 
+  const optionTab = ({ optionName, viewMode }) => {
+    return (
+      <div
+        className={classNames(styles.optionContainer, {
+          [styles.currentOption]: profileViewMode === viewMode,
+        })}
+        onClick={() => changeProfileViewMode(viewMode)}
+      >
+        {optionName}
+      </div>
+    );
+  };
+
   return (
     <div>
       <Header />
@@ -28,24 +41,14 @@ const UserProfile = (props) => {
           <div className={styles.aside}>
             <span className={styles.headerAside}>Select Option</span>
             <div className={styles.optionsContainer}>
-              <div
-                className={classNames(styles.optionContainer, {
-                  [styles.currentOption]:
-                    profileViewMode === CONSTANTS.USER_INFO_MODE,
-                })}
-                onClick={() => changeProfileViewMode(CONSTANTS.USER_INFO_MODE)}
-              >
-                UserInfo
-              </div>
-              <div
-                className={classNames(styles.optionContainer, {
-                  [styles.currentOption]:
-                    profileViewMode === CONSTANTS.CASHOUT_MODE,
-                })}
-                onClick={() => changeProfileViewMode(CONSTANTS.CASHOUT_MODE)}
-              >
-                Cashout
-              </div>
+              {optionTab({
+                optionName: 'Profile',
+                viewMode: CONSTANTS.USER_INFO_MODE,
+              })}
+              {optionTab({
+                optionName: 'Cashout',
+                viewMode: CONSTANTS.CASHOUT_MODE,
+              })}
             </div>
           </div>
         )}
@@ -66,7 +69,6 @@ const UserProfile = (props) => {
 };
 
 const mapStateToProps = (state) => {
-  // const { balance, role } = state.userStore?.data;
   const { profileViewMode } = state.userProfile;
   const { error } = state.payment;
 

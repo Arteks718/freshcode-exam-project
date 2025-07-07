f01af2b284ec7011566e1e83e9ba95288b9719c0
Arteks718 <artexbro.official@gmail.com>
2025-07-07 13:44:14 +0300

    Refactor SlideBar & Replace flickity to swiper

--- client/src/components/SlideBar/SlideBar.jsx
+++ client/src/components/SlideBar/SlideBar.jsx
@@ -1,68 +1,110 @@
-import React from 'react';
-import Flickity from 'react-flickity-component';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation } from 'swiper/modules';
 import style from './SlideBar.module.sass';
 import carouselConstants from '../../carouselConstants';
-import './flickity.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
-const SliderBar = props => {
-  const options = {
-    draggable: true,
-    wrapAround: true,
-    pageDots: false,
-    prevNextButtons: true,
-    autoPlay: true,
-    groupCells: true,
-    lazyLoad: true,
-  };
+const {
+  MAIN_SLIDER,
+  EXAMPLE_SLIDER,
+  FEEDBACK_SLIDER,
+  EXAMPLE_SLIDER_TEXT,
+  FEEDBACK_SLIDER_TEXT,
+} = carouselConstants;
 
-  const getStyleName = () => {
-    const { carouselType } = props;
-    switch (carouselType) {
-      case carouselConstants.MAIN_SLIDER:
-        return style.mainCarousel;
-      case carouselConstants.EXAMPLE_SLIDER:
-        return style.exampleCarousel;
-      case carouselConstants.FEEDBACK_SLIDER:
-        return style.feedbackCarousel;
-    }
-  };
+const containerStyleName = {
+  MAIN_SLIDER: style.mainCarousel,
+  EXAMPLE_SLIDER: style.exampleCarousel,
+  FEEDBACK_SLIDER: style.feedbackCarousel,
+};
 
-  const renderSlides = () => {
-    const { carouselType } = props;
-    switch (carouselType) {
-      case carouselConstants.MAIN_SLIDER: {
-        return Object.keys(props.images).map((key, index) => (
+const getSwiperSettings = (carouselType) => {
+  switch (carouselType) {
+    case MAIN_SLIDER:
+      return {
+        breakpoints: {
+          950: { slidesPerView: 3, centeredSlides: true, spaceBetween: 30 },
+          480: { slidesPerView: 2, centeredSlides: false, spaceBetween: 20 },
+          0: { slidesPerView: 1, spaceBetween: 0 },
+        },
+      };
+    case EXAMPLE_SLIDER:
+      return {
+        breakpoints: {
+          950: { slidesPerView: 3, centeredSlides: true, spaceBetween: 30 },
+          480: { slidesPerView: 2, centeredSlides: false, spaceBetween: 20 },
+          0: { slidesPerView: 1, spaceBetween: 0 },
+        },
+      };
+    case FEEDBACK_SLIDER:
+      return {
+        centeredSlides: true,
+        breakpoints: {
+          950: { slidesPerView: 4, spaceBetween: 30 },
+          680: { slidesPerView: 3, spaceBetween: 20 },
+          480: { slidesPerView: 2, spaceBetween: 20, centeredSlides: true },
+          0: { slidesPerView: 1, centeredSlides: false, spaceBetween: 0 },
+        },
+      };
+    default:
+      return { slidesPerView: 1, centeredSlides: false };
+  }
+};
+
+const renderSlides = ({ carouselType, images }) => {
+  switch (carouselType) {
+    case MAIN_SLIDER:
+      return Object.keys(images).map((key, index) => (
+        <SwiperSlide key={index}>
           <img
-            src={props.images[key]}
-            alt='slide'
-            key={index}
-            className={style['carousel-cell']}
+            src={images[key]}
+            alt="slide"
+            className={style.carouselCell}
+            loading="lazy"
           />
-        ));
-      }
-      case carouselConstants.EXAMPLE_SLIDER: {
-        return Object.keys(props.images).map((key, index) => (
-          <div className={style['example-cell']} key={index}>
-            <img src={props.images[key]} alt='slide' />
-            <p>{carouselConstants.EXAMPLE_SLIDER_TEXT[index]}</p>
+        </SwiperSlide>
+      ));
+    case EXAMPLE_SLIDER:
+      return Object.keys(images).map((key, index) => (
+        <SwiperSlide key={index}>
+          <div className={style.exampleCell}>
+            <img src={images[key]} alt="slide" loading="lazy" />
+            <p>{EXAMPLE_SLIDER_TEXT[index]}</p>
           </div>
-        ));
-      }
-      case carouselConstants.FEEDBACK_SLIDER: {
-        return Object.keys(props.images).map((key, index) => (
-          <div className={style['feedback-cell']} key={index}>
-            <img src={props.images[key]} alt='slide' />
-            <p>{carouselConstants.FEEDBACK_SLIDER_TEXT[index].feedback}</p>
-            <span>{carouselConstants.FEEDBACK_SLIDER_TEXT[index].name}</span>
+        </SwiperSlide>
+      ));
+    case FEEDBACK_SLIDER:
+      return Object.keys(images).map((key, index) => (
+        <SwiperSlide key={index} style={{ display: 'flex', height: 'auto' }}>
+          <div className={style.feedbackCell}>
+            <img src={images[key]} alt="slide" loading="lazy" />
+            <p>{FEEDBACK_SLIDER_TEXT[index].feedback}</p>
+            <span>{FEEDBACK_SLIDER_TEXT[index].name}</span>
           </div>
-        ));
-      }
-    }
-  };
+        </SwiperSlide>
+      ));
+    default:
+      return null;
+  }
+};
+
+const SliderBar = ({ carouselType, images }) => {
+  const swiperSettings = getSwiperSettings(carouselType);
+
   return (
-    <Flickity className={getStyleName()} elementType='div' options={options}>
-      {renderSlides()}
-    </Flickity>
+    <Swiper
+      {...swiperSettings}
+      loop={true}
+      navigation={true}
+      spaceBetween={30}
+      autoplay={{ delay: 2500, disableOnInteraction: true }}
+      lazy={true}
+      modules={[Autoplay, Navigation]}
+      className={containerStyleName[carouselType]}
+    >
+      {renderSlides({ carouselType, images })}
+    </Swiper>
   );
 };
 
